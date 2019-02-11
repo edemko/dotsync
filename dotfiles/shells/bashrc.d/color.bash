fafd7f16b09925f5ff30baab13aa75e016ea6ad2
Okuno Zankoku <zankoku.okuno@gmail.com>
2019-02-10 21:04:19 -0500

    split up the main bashrc file

---
+++ dotfiles/shells/bashrc.d/color.bash
@@ -0,0 +1,80 @@
+# detect if this terminal support color
+# (whitelisted, so non-color, unless we know we "want" color)
+case "$TERM" in
+    xterm|xterm-color|*-256color) color_term=yes;;
+esac
+
+
+if [ "$color_term" = yes ]; then
+    # enable color support of ls and also add handy aliases
+    if [ -x /usr/bin/dircolors ]; then
+        [ -r ~/.dircolors ] && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+        alias ls='ls --color=auto'
+
+        alias grep='grep --color=auto'
+        alias fgrep='fgrep --color=auto'
+        alias egrep='egrep --color=auto'
+    fi
+
+    # colored GCC warnings and errors
+    export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+fi
+
+
+# set shortcuts for color escapes
+if [ "$color_term" = yes ]; then
+    col_CLR="\e[0m"
+
+    col_NO="\e[39m"
+    col_BLA="\e[30m"
+    col_RED="\e[31m"
+    col_GRE="\e[32m"
+    col_YEL="\e[33m"
+    col_BLU="\e[34m"
+    col_MAG="\e[35m"
+    col_CYA="\e[36m"
+    col_LGRA="\e[37m"
+    col_GRA="\e[90m"
+    col_LRED="\e[91m"
+    col_LGRE="\e[92m"
+    col_LYEL="\e[93m"
+    col_LBLU="\e[94m"
+    col_LMAG="\e[95m"
+    col_LCYA="\e[96m"
+    col_WHI="\e[97m"
+
+    bkg_NO="\e[49m"
+    bkg_BLA="\e[40m"
+    bkg_RED="\e[41m"
+    bkg_GRE="\e[42m"
+    bkg_YEL="\e[43m"
+    bkg_BLU="\e[44m"
+    bkg_MAG="\e[45m"
+    bkg_CYA="\e[46m"
+    bkg_LGRA="\e[47m"
+    bkg_GRA="\e[100m"
+    bkg_LRED="\e[101m"
+    bkg_LGRE="\e[102m"
+    bkg_LYEL="\e[103m"
+    bkg_LBLU="\e[104m"
+    bkg_LMAG="\e[105m"
+    bkg_LCYA="\e[106m"
+    bkg_WHI="\e[107m"
+
+    sty_BOLD="\e[1m"
+    sty_DIM="\e[2m"
+    sty_ULINE="\e[4m"
+    sty_BLINK="\e[5m"
+    sty_INV="\e[7m"
+    sty_HIDE="\e[8m"
+
+    sty_NOBOLD="\e[21m"
+    sty_NODIM="\e[22m"
+    sty_NOULINE="\e[24m"
+    sty_NOBLINK="\e[25m"
+    sty_NOINV="\e[27m"
+    sty_NOHIDE="\e[28m"
+fi
+
+
+unset color_term
---
+++ dotfiles/shells/bashrc.d/prompt.bash
@@ -0,0 +1,28 @@
+# always clear any color
+PS1="\[${col_CLR}\]"
+# show last exit code if non-zero
+__prompt_failcode() {
+    (
+        ec="$?"
+        if [[ $ec != 0 ]]; then
+            echo "<${ec}> "
+        fi
+    )
+}
+PS1+="\[${col_RED}\]\$(__prompt_failcode)\[${col_CLR}\]"
+# number of background jobs
+PS1+="\[${col_GRA}\][j:\j]\[${col_CLR}\]"
+# chroot
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+PS1+="\[${sty_BOLD}${col_YEL}\]"'${debian_chroot:+($debian_chroot)}'"\[${col_CLR}\]"
+# user, host, and working directory
+PS1+="\[${col_BLU}\]\u@\h:\[${col_LBLU}\]\w\[${col_CLR}\]"
+# dollar or hash for prompt
+PS1+="\[${sty_BOLD}${sty_DIM}${col_WHI}\]\$\[${col_CLR}\] "
+
+# always clear any color
+PS2="\[${col_CLR}\]"
+# a simple caret is enough
+PS2+="\[${sty_BOLD}${sty_DIM}${col_WHI}\]>\[${col_CLR}\] "
