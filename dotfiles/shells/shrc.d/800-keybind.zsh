76c433b4b5694eae7a0245a52f541142064b6841
Okuno Zankoku <zankoku.okuno@gmail.com>
2019-02-12 19:00:09 -0500

    switch zsh away from vi bindings

--- dotfiles/shells/shrc.d/800-keybind.zsh
+++ dotfiles/shells/shrc.d/800-keybind.zsh
@@ -1,6 +1,5 @@
-# FIXME do I want vi, or just stick with emacs?
-bindkey -v
+bindkey -e
 bindkey "^[[1;5C" forward-word
 bindkey "^[[1;5D" backward-word
-bindkey '^R' history-incremental-search-backward
+
 setopt ignoreeof
