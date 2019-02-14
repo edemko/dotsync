f86cbb956197fb3c6454471a3fd1e5b5a6e996cb
Okuno Zankoku <zankoku.okuno@gmail.com>
2019-02-14 14:09:20 -0500

    sqlite config

---
+++ dotfiles/sqlite/install.sh
@@ -0,0 +1,20 @@
+sqliterc="${HOME}/.sqliterc"
+sqliterc_src="${1}/sqliterc"
+
+
+dotsync_depsgood() {
+    which sqlite3 >/dev/null 2>&1
+}
+
+dotsync_newest() {
+    diff -q 2>/dev/null "${sqliterc}" "${sqliterc_src}" || return 1
+}
+
+dotsync_setup() {
+    ln -svf "${sqliterc_src}" "${sqliterc}"
+}
+
+dotsync_teardown() {
+    [ -L "${sqliterc}" ] && rm -v "${sqliterc}"
+    return 0
+}
