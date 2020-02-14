79895564826606dec1d252ee109f2727da70fcba
Eric Demko <edemko@layer3com.com>
2020-02-14 10:54:57 -0500

    primitve (i.e. lots of assumptions) color support
      * only handling the 256 color case for withaf
      * no good argparsing for withaf
      * should also have the ability to set background color and attributes
        alongside foreground
      * if too many colors are supported, testing color will dump
        an infeasible amount of output
      * none of the modules are using colors yet

---
+++ dotfiles/dotsync/colors.sh
@@ -0,0 +1,1 @@
+../../bin/colors.sh
--- dotfiles/dotsync/install.sh
+++ dotfiles/dotsync/install.sh
@@ -1,20 +1,24 @@
 srcexe="${1}/dotsync"
 srcfuncs="${1}/dotfuncs"
+srccolors="${1}/colors.sh"
 bindir="${HOME}/bin"
 targetexe="${bindir}/dotsync"
 targetfuncs="${bindir}/dotfuncs"
+targetcolors="${bindir}/colors.sh"
 
 dotsync_depsgood() { return 0 ; }
 dotsync_newest() {
-    [ -x "${targetexe}" ] && [ -f "${targetfuncs}" ]
+    [ -x "${targetexe}" ] && [ -f "${targetfuncs}" ] && [ -f "${targetcolors}" ]
 }
 dotsync_setup() {
     [ -d "${bindir}" ] || mkdir -v "${bindir}"
     ln -svf "${srcexe}" "${targetexe}"
     ln -svf "${srcfuncs}" "${targetfuncs}"
+    ln -svf "${srccolors}" "${targetcolors}"
 }
 dotsync_teardown() {
     [ -L "${targetexe}" ] && rm -v "${targetexe}"
     [ -L "${targetfuncs}" ] && rm -v "${targetfuncs}"
+    [ -L "${targetfuncs}" ] && rm -v "${targetcolors}"
     return 0
 }
