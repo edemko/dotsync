a1771fbcb7fb794ddf6312bb8e173acd59df333f
Okuno Zankoku <zankoku.okuno@gmail.com>
2020-02-19 20:03:52 -0500

    git install asks for user info if necessary

--- dotfiles/git/install.sh
+++ dotfiles/git/install.sh
@@ -4,6 +4,8 @@ dotsync_depsgood() {
 
 dotsync_newest() {
     git config --global --get core.editor >/dev/null || return 1
+    git config --global --get user.name >/dev/null || return 1
+    git config --global --get user.email >/dev/null || return 1
     # Aliases
     git config --global --get alias.st >/dev/null || return 1
     git config --global --get alias.graph >/dev/null || return 1
@@ -15,7 +17,24 @@ dotsync_newest() {
 }
 
 dotsync_setup() {
+    local inp
     git config --global core.editor vim
+    if ! git config --global --get user.name >/dev/null; then
+        echo >&2 "$(withaf d0f '[NOTICE]') Global git user.name not set; who are you [skip]: "
+        read inp
+        case "${inp}" in
+            '') ;;
+            * ) git config --global user.name "${inp}" ;;
+        esac
+    fi
+    if ! git config --global --get user.email >/dev/null; then
+        echo >&2 "$(withaf d0f '[NOTICE]') Global git user.email not set; what's your email [skip]: "
+        read inp
+        case "${inp}" in
+            '') ;;
+            * ) git config --global user.email "${inp}" ;;
+        esac
+    fi
     # Aliases
     git config --global alias.st 'status'
     git config --global alias.graph 'log --graph --all --oneline --decorate'
