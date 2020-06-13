f8223d3a3a81584fcb1ae327895d50babbc4287b
Okuno Zankoku <zankoku.okuno@gmail.com>
2020-06-13 13:59:40 -0400

    gnome-terminal configs

---
+++ dotfiles/gnome-terminal/install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+preferences_src="${1}/preferences.dconf"
+
+
+dotsync_depsgood() {
+    if ! which gnome-terminal >/dev/null; then
+        echo >&2 "$(withaf f00 '[ERROR]') gnome-terminal is not installed"
+        return 1
+    fi
+    if ! which runghc >/dev/null; then
+        echo >&2 "$(withaf f00 '[ERROR]') dconf is not installed"
+        return 1
+    fi
+    return 0
+}
+
+dotsync_newest() {
+    if ! (dconf dump /org/gnome/terminal/ | diff -q 2>/dev/null - "${preferences_src}"); then
+        return 1
+    fi
+    return 0
+}
+
+dotsync_setup() {
+    dconf load /org/gnome/terminal/ <"${preferences_src}"
+    return 0
+}
+
+dotsync_teardown() {
+    echo >&2 "$(withaf f80 '[WARNING]') teardown unimplemented"
+    return 0
+}
