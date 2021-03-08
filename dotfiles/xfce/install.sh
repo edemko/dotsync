0e309d026148ab6cec39c3e5c050c8c6d253d4f6
Okuno Zankoku <zankoku.okuno@gmail.com>
2021-03-07 20:10:17 -0500

    xfce syncing; don't rely on system XCompose settings

--- dotfiles/xfce/install.sh
+++ dotfiles/xfce/install.sh
@@ -1,26 +1,36 @@
 #!/bin/sh
 
-src="${dotdir}/src"
-target="${HOME}/.config/xfce4/xfconf/xfce-perchannel-xml"
-bakup="${target}.bak"
+srcdir="${dotdir}/src"
+targetdir="${HOME}/.config/xfce4/xfconf/xfce-perchannel-xml"
+backupdir="${targetdir}.bak"
+syncd_files='xfce4-keyboard-shortcuts.xml xfwm4.xml xsettings.xml'
 
 dotsync_depsgood() {
+    return 0
 }
 
 dotsync_newest() {
-    [ -d "$target" -a -L "$target" ] || return 1
+    for file in $syncd_files; do
+        [ -L "$targetdir/$file" ] || return 1
+    done
 }
 
 dotsync_setup() {
-    if [ -d "$target" -a ! -L "$target" ]; then
-        echo >&2 "$(withaf d0f '[NOTICE]') creating back-up of '${target}' at '${backup}'"
-        mv "${target}" "${backup}"
-    fi
-    ln -svf "${src}" "${target}"
+    for file in $syncd_files; do
+        if [ ! -L "$target/$file" ]; then
+            echo >&2 "$(withaf d0f '[NOTICE]') creating back-up of '$targetdir/$file' at '$backupdir/$file'"
+            mkdir -p "$backupdir"
+            mv "$targetdir/$file" "$backupdir/$file"
+        fi
+        ln -svf "$srcdir/$file" "$targetdir/$file"
+    done
 }
 
 dotsync_teardown() {
-    [ -L "$target" ] && rm "$target"
-    [ -d "$backup" ] && mv "$backup" "$target"
+    for file in $syncd_files; do
+        [ -L "$targetdir/$file" ] && rm "$targetdir/$file"
+        [ -e "$backupdir/$file" ] && mv "$backupdir/$file" "$targetdir/$file"
+    done
+    [ -d "$backupdir" ] && rm -r "$backupdir"
     return 0
 }
