875b1982c7804b9c561b7933331a8e8318519f57
Okuno Zankoku <zankoku.okuno@gmail.com>
2021-03-07 21:34:28 -0500

    revised xfce syncing

--- dotfiles/xfce/install.sh
+++ dotfiles/xfce/install.sh
@@ -3,34 +3,30 @@
 srcdir="${dotdir}/src"
 targetdir="${HOME}/.config/xfce4/xfconf/xfce-perchannel-xml"
 backupdir="${targetdir}.bak"
-syncd_files='xfce4-keyboard-shortcuts.xml xfwm4.xml xsettings.xml'
+syncd_files='xfce4-desktop.xml xfce4-keyboard-shortcuts.xml xfce4-panel.xml xfwm4.xml xsettings.xml'
 
 dotsync_depsgood() {
     return 0
 }
 
 dotsync_newest() {
+    local ret=0
     for file in $syncd_files; do
-        [ -L "$targetdir/$file" ] || return 1
+        diff -q "$targetdir/$file" "$srcdir/$file" || ret=1
     done
+    return "$ret"
 }
 
 dotsync_setup() {
     for file in $syncd_files; do
-        if [ ! -L "$target/$file" ]; then
-            echo >&2 "$(withaf d0f '[NOTICE]') creating back-up of '$targetdir/$file' at '$backupdir/$file'"
+        if ! diff -q >/dev/null "$target/$file" "$srcdir/$file"; then
             mkdir -p "$backupdir"
-            mv "$targetdir/$file" "$backupdir/$file"
+            cp -v "$targetdir/$file" "$backupdir/$file"
+            cp -v "$srcdir/$file" "$targetdir/$file"
         fi
-        ln -svf "$srcdir/$file" "$targetdir/$file"
     done
 }
 
 dotsync_teardown() {
-    for file in $syncd_files; do
-        [ -L "$targetdir/$file" ] && rm "$targetdir/$file"
-        [ -e "$backupdir/$file" ] && mv "$backupdir/$file" "$targetdir/$file"
-    done
-    [ -d "$backupdir" ] && rm -r "$backupdir"
     return 0
 }
